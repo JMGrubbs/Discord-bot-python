452e2793ccb912ea1c2ee27e16fa9d2cf4b15df8
John <johnmlowery@gmail.com>
2023-11-28 19:21:00 -0700

    css formatting

--- bot_chat/src/chatapp/ChatApp.js
+++ bot_chat/src/chatapp/ChatApp.js
@@ -4,19 +4,21 @@ import Message from './Message';
 function ChatApp() {
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState('');
+    const [sender, setSender] = useState('');
 
     const handleSendMessage = () => {
         if (newMessage) {
-            setMessages([...messages, { text: newMessage }]);
+            setMessages([...messages, { text: newMessage, sender: sender }]);
             setNewMessage('');
         }
     };
+
 
     return (
         <div className="chat-app">
             <div className="message-list">
                 {messages.map((message, index) => (
-                    <Message key={index} text={message.text} />
+                    <Message key={index} text={message.text} sender={message.sender} />
                 ))}
             </div>
             <div className="message-input">
@@ -24,7 +26,10 @@ function ChatApp() {
                     type="text"
                     placeholder="Type your message..."
                     value={newMessage}
-                    onChange={(e) => setNewMessage(e.target.value)}
+                    onChange={(e) => {
+                        setSender('user')
+                        setNewMessage(e.target.value)
+                    }}
                 />
                 <button onClick={handleSendMessage}>Send</button>
             </div>
--- bot_chat/src/chatapp/Message.js
+++ bot_chat/src/chatapp/Message.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function Message({ text }) {
-    return <div className="message">{text}</div>;
+function Message({ sender, text }) {
+    // Determine the additional class based on the sender
+    const senderClass = sender === 'agent' ? 'agent' : 'user';
+
+    // Apply both `message` and the sender-specific class
+    return (
+        <div className={`message sender ${senderClass}`}>
+            {text}
+        </div>
+    );
 }
 
 export default Message;
