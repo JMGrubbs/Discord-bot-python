1e482d351d82d371a043577ab658980f15db2032
John <johnmlowery@gmail.com>
2023-11-28 18:13:58 -0700

    frontend styling

--- bot_chat/src/App.js
+++ bot_chat/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './App.css';
-import './Sidebar.css';
-import ChatApp from './ChatApp';
-import Sidebar from './Sidebar';
+import './css/App.css';
+import './css/Sidebar.css';
+import ChatApp from './chatapp/ChatApp';
+import Sidebar from './sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Simple Autogen Clone ChatApp</h1>
+        <h1>Simple Autogen Clone</h1>
       </header>
       <main className='app-holder'>
         <Sidebar />
